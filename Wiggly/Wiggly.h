c6d185a837b096316404fcfd5ccce423f73616dd
KptainO <kptaino@gmail.com>
2013-06-06 23:36:54 +0200

    [project] added global header file

---
+++ Wiggly/Wiggly.h
@@ -0,0 +1,12 @@
+//
+// This file is part of  Wiggly project
+//
+// Created by JC on 09/06/13.
+// For the full copyright and license information, please view the LICENSE
+// file that was distributed with this source code.
+//
+
+#import <Wiggly/WIRouter.h>
+#import <Wiggly/WIRoute.h>
+#import <Wiggly/WIRouteBuilder.h>
+#import <Wiggly/WIRoutePlaceholder.h>
